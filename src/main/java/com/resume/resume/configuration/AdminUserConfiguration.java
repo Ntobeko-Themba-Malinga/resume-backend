f565d4fcfc9680d6676bcad2c4fd0aeb0129ce1a
unknown <ntobekothe2@gmail.com>
2024-01-19 19:44:03 +0200

    Admin password BCrypt encoding fix

--- src/main/java/com/resume/resume/configuration/AdminUserConfiguration.java
+++ src/main/java/com/resume/resume/configuration/AdminUserConfiguration.java
@@ -6,6 +6,7 @@ import com.resume.resume.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Optional;
 
@@ -20,7 +21,7 @@ public class AdminUserConfiguration {
             if (user.isEmpty()) {
                 User adminUser = new User(
                         "admin@admin.com",
-                        "admin",
+                        new BCryptPasswordEncoder().encode("admin"),
                         Role.ADMIN
                 );
                 userRepository.save(adminUser);
